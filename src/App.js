563bc53ca11848927debeb0972d226316c7839ce
congcuong <congucong095@gmail.com>
2022-06-23 16:45:41 +0700

    23-6 commit 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
+    );
 }
 
 export default App;
