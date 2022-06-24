cb5eb09a2bc7074e124d4873d254c409341816d5
congcuong <congucong095@gmail.com>
2022-06-24 16:59:33 +0700

    24-6 commit 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '@/routes';
+import Header from '@/components/Header/Header';
+import Login from '@/pages/Login';
+
 function App() {
-    return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
-    );
+    let login = false;
+
+    if (login) {
+        return (
+            <Router>
+                <div className="App">
+                    <Header />
+                    <Routes>
+                        {publicRoutes.map((route, index) => {
+                            const Page = route.component;
+                            return <Route key={index} path={route.path} element={<Page />} />;
+                        })}
+                    </Routes>
+                </div>
+            </Router>
+        );
+    } else {
+        return <Login />;
+    }
 }
 
 export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import './index.scss';
+
+function Login() {
+    return (
+        <div className="container">
+            <div className="content">
+                <div className="slider"></div>
+                <div className="login"></div>
+            </div>
+            <div className="footer"></div>
+        </div>
+    );
+}
+
+export default Login;
