448be054b2dc17eabdb6cd3e6be41384c0742b58
congcuong <congucong095@gmail.com>
2022-06-25 16:39:44 +0700

    25-6 commit 2

---
+++ src/assets/images/index.js
@@ -0,0 +1,12 @@
+const images = {
+    logo: require('@/assets/images/logo.png'),
+    appstore: require('@/assets/images/appstore.png'),
+    googleplay: require('@/assets/images/googleplay.png'),
+    loginBackground: require('@/assets/images/login-background.png'),
+    loginSlide1: require('@/assets/images/login-slide-1.png'),
+    loginSlide2: require('@/assets/images/login-slide-2.png'),
+    loginSlide3: require('@/assets/images/login-slide-3.png'),
+    loginSlide4: require('@/assets/images/login-slide-4.png'),
+};
+
+export default images;
--- src/images/index.js
+++
@@ -1,5 +0,0 @@
-const images = {
-    loginSlide1: require('./login-slide-1.png').default,
-};
-
-export default images;
