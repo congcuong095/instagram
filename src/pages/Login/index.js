80ce50bdab9e95fda8de626d86cdd47a063af735
congucong095 <congcuong095@gmail.com>
2022-06-27 00:36:44 +0700

    27-6 commit 1

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,8 +2,7 @@ import styles from './Login.module.scss';
 import classNames from 'classnames/bind';
 import images from '@/assets/images';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import Account from '@/components/Account';
 
 const cx = classNames.bind(styles);
 
@@ -14,38 +13,7 @@ function Login() {
                 <div className={cx('slider')}>
                     <img className={cx('slider-img')} src={images.loginSlide1} alt="" />
                 </div>
-                <div className={cx('login')}>
-                    <div className={cx('login-main')}>
-                        <div className={cx('login-header')}>
-                            <img className={cx('login-logo')} src={images.logo} alt="insatgram" />
-                        </div>
-                        <div className={cx('login-input')}>
-                            <input
-                                className={cx('login-input-name')}
-                                type="text"
-                                placeholder="Số điện thoại, tên người dùng hoặc email"
-                            />
-                            <input className={cx('login-input-password')} type="password" placeholder="Mật khẩu" />
-                            <button className={cx('login-btn')}>Đăng nhập</button>
-                        </div>
-                        <div className={cx('login-seperate')}>HOẶC</div>
-                        <div className={cx('login-info')}>
-                            <div className={cx('login-facebook')}>
-                                <FontAwesomeIcon icon={faFacebookSquare} />
-                                <div>Đăng nhập bằng Facebook</div>
-                            </div>
-                            <div className={cx('login-forgot-password')}>Quên mật khẩu</div>
-                        </div>
-                    </div>
-                    <div className={cx('login-register')}>
-                        Bạn chưa có tài khoản ư? <a href="/">Đăng ký</a>
-                    </div>
-                    <div className={cx('login-app')}>
-                        <div className={cx('login-app-title')}>Tải ứng dụng</div>
-                        <img src={images.appstore} alt="App Store" />
-                        <img src={images.googleplay} alt="Google Play" />
-                    </div>
-                </div>
+                <Account haveAccount />
             </div>
             <div className={cx('footer')}></div>
         </div>
