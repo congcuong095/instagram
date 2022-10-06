7fd2f98276f3857a3a196f67cffda9d6d1cc2528
congcuong <congucong095@gmail.com>
2022-10-06 16:10:23 +0700

    6/10 commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { publicRoutes, privateRoutes } from '@/routes';
 import { useEffect, useState } from 'react';
 import { useLocalStore } from './hooks';
